3bf6b7e24e2b7dc1bea17396e9c039eda3f76270
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-14 15:08:44 -0400

    feat: add basic hello world

---
+++ src/server/main.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+class Server {
+    public static void main(String[] args) throws Exception {
+        final String PORT = System.getenv("PORT");
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT != null ? Integer.parseInt(PORT) : 8080), 0);
+        server.createContext("/", new HelloWorld());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class HelloWorld implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello World!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
