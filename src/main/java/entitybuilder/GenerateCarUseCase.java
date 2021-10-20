df431c2f777293fa3343ef5fa902612312930bf4
Mogtaba Awad <maximus@Mogtabas-MacBook-Pro.local>
2021-10-20 17:02:09 -0400

    feat: Created a class that takes in a car name and and year and creates a Car object

---
+++ src/main/java/entitybuilder/GenerateCarUseCase.java
@@ -0,0 +1,19 @@
+package entitybuilder;
+
+import entities.Car;
+
+/**
+ * A class to generate a Car object
+ * upon request from the controller
+ */
+public class GenerateCarUseCase {
+    /**
+     *
+     * @param name The name of the car being bought
+     * @param year The year of the car model
+     * @return A new Car object with the provided name and model year
+     */
+    public Car GenerateCarUseCase(String name, int year){
+        return new Car(0, name, year);
+    }
+}
