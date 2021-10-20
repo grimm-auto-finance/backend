82006abdb87392774ffb606e62f79b516dd7fb3d
Mogtaba Awad <maximus@Mogtabas-MacBook-Pro.local>
2021-10-20 18:01:44 -0400

    fix: fixed bracket spacing in GenerateAddOnsUseCase.java

--- src/main/java/entitybuilder/GenerateAddOnsUseCase.java
+++ src/main/java/entitybuilder/GenerateAddOnsUseCase.java
@@ -16,14 +16,14 @@ public class GenerateAddOnsUseCase {
      * @param ListOfAddOnNames A list containg names of Addons that we want to generate
      * @return A mapping of addon name to its corresponding AddOn object
      */
-    public HashMap<String, AddOn> GenerateAddOnsUseCase(List<String> ListOfAddOnNames){
+    public HashMap<String, AddOn> GenerateAddOnsUseCase(List<String> ListOfAddOnNames) {
         /**
          * A mapping of the name of the addons we get from the list of addons in
          * our parameter to the AddOn objects with the corresponding name
          * which we create below
          */
         HashMap<String, AddOn> MapOfAddOns = new HashMap<String, AddOn>();
-        for (String addOnName: ListOfAddOnNames){
+        for (String addOnName: ListOfAddOnNames) {
             /**
              * TODO: Find out if we want to set the price and description of an addon as parameters to the constructor
              * because they will always begin as 0 and empty string as this information is obtained from the
