4313f2e2ac5c1050c9902cc5ebcc7200c023cc95
Mogtaba Awad <maximus@Mogtabas-MacBook-Pro.local>
2021-10-20 17:02:52 -0400

    feat: Created a class that generates an empty LoanData object

---
+++ src/main/java/entitybuilder/GenerateLoanUseCase.java
@@ -0,0 +1,16 @@
+package entitybuilder;
+
+import entities.LoanData;
+
+/**
+ * CLass to generate an empty LoanData object
+ */
+public class GenerateLoanUseCase {
+    /**
+     * Constructor that generates the empty LoanData and returns it
+     * @return Returns an empty Loan Data object
+     */
+    public LoanData GenerateLoanDataUseCase(){
+        return new LoanData();
+    }
+}
