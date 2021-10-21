af7f20f614c1472ed120efbebac260d26c92aef2
Gabe Guralnick <gnguralnick@gmail.com>
2021-10-20 22:43:39 -0400

    updated ParseAddOnsUseCase
    main method is commented out since we don't need it for the demo

--- src/main/java/entityparsers/ParseAddOnsUseCase.java
+++ src/main/java/entityparsers/ParseAddOnsUseCase.java
@@ -1,4 +1,33 @@
 package entityparsers;
 
+import entities.AddOn;
+import entities.AddOns;
+import entitybuilder.GenerateAddOnsUseCase;
+import entitybuilder.GenerateBuyerUseCase;
+
+import javax.json.JsonObject;
+import java.util.Map;
+
 public class ParseAddOnsUseCase {
+    private final JsonObject jsonObject;
+
+    /**
+     * Constructs a new ParseAddOnsUseCase to create a AddOns
+     * using the given JsonObject
+     * @param jsonObject
+     */
+    public ParseAddOnsUseCase(JsonObject jsonObject) {
+        this.jsonObject = jsonObject;
+    }
+
+    /**
+     * Creates a map from add-on names to AddOn objects from the fields in jsonObject
+     * @return
+     */
+//    public Map<String, AddOn> parse() {
+//        GenerateAddOnsUseCase addOnsGenerator = new GenerateAddOnsUseCase();
+//        JsonObject addOnsObject = jsonObject.getJsonObject("add-ons");
+//
+//        return addOnsGenerator.GenerateAddOnsUseCase();
+//    }
 }
