08a4ff20cb96353982bc468c830740e5990fca84
Gabe Guralnick <gnguralnick@gmail.com>
2021-10-21 18:55:58 -0400

    feat: update Loan.java to use parsers from payload json

--- src/main/java/controllers/routes/Loan.java
+++ src/main/java/controllers/routes/Loan.java
@@ -5,13 +5,12 @@ import entities.CarBuyer;
 import entities.LoanData;
 import entitypackagers.PackageAllUseCase;
 import entitypackagers.PackageLoanDataUseCase;
+import entityparsers.ParseCarBuyerUseCase;
+import entityparsers.ParseCarUseCase;
 import fetchers.LoanDataFetcher;
 import constants.Exceptions;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+
+import java.io.*;
 import java.net.HttpURLConnection;
 import com.sun.net.httpserver.HttpExchange;
 import javax.json.*;
@@ -31,15 +30,38 @@ public class Loan extends controllers.Route {
 
         OutputStream os = t.getResponseBody();
 
+        InputStream is = t.getRequestBody();
+        JsonReader jsonReader = Json.createReader(is);
+        Car car;
+        CarBuyer buyer;
         try {
-            Car car = new Car("Honda", "Civic", 2002);
-            car.setPrice(7000);
-            CarBuyer carBuyer = new CarBuyer(30000.0, 780);
-            LoanData loanData = LoanDataFetcher.fetch(carBuyer, car);
+            JsonObject inputObj = jsonReader.readObject();
+            ParseCarUseCase carParser = new ParseCarUseCase(inputObj);
+            ParseCarBuyerUseCase buyerParser = new ParseCarBuyerUseCase(inputObj);
+            car = carParser.parse();
+            buyer = buyerParser.parse();
+            if (car.getMake() == null || car.getModel() == null) {
+                String message = "Error in Payload JSON parsing";
+                t.sendResponseHeaders(400, message.length());
+                os.write(message.getBytes());
+                os.close();
+                return;
+            }
+        } catch (NullPointerException | ClassCastException e) {
+            String message = "Error in Payload JSON parsing";
+            t.sendResponseHeaders(400, message.length());
+            os.write(message.getBytes());
+            os.close();
+            return;
+        }
+
+
+        try {
+            LoanData loanData = LoanDataFetcher.fetch(buyer, car);
 
             JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
             PackageAllUseCase allPackager = new PackageAllUseCase(jsonBuilder);
-            allPackager.writeEntities(car, carBuyer, loanData);
+            allPackager.writeEntities(car, buyer, loanData);
             String responseString = jsonBuilder.build().toString();
             t.sendResponseHeaders(200, responseString.length());
             os.write(responseString.getBytes());
