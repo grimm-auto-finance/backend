cba46752733c6fef034fffd060bf0d453b4dcfb2
Gabe Guralnick <gnguralnick@gmail.com>
2021-10-20 22:11:46 -0400

    feat: update ParseCarUseCase
    reads car info from json
    notably, uses the Car constructor without a price since car price might not be known yet

--- src/main/java/entityparsers/ParseCarUseCase.java
+++ src/main/java/entityparsers/ParseCarUseCase.java
@@ -1,4 +1,33 @@
 package entityparsers;
 
+import entities.Car;
+import entitybuilder.GenerateBuyerUseCase;
+import entitybuilder.GenerateCarUseCase;
+
+import javax.json.JsonObject;
+
 public class ParseCarUseCase {
+    private final JsonObject jsonObject;
+
+    /**
+     * Constructs a new ParseCarUseCase to create a Car
+     * using the given JsonObject
+     * @param jsonObject
+     */
+    public ParseCarUseCase(JsonObject jsonObject) {
+        this.jsonObject = jsonObject;
+    }
+
+    /**
+     * Creates a Car object from the fields in jsonObject
+     * @return
+     */
+    public Car parse() {
+        GenerateCarUseCase carGenerator = new GenerateCarUseCase();
+        JsonObject carObj = jsonObject.getJsonObject("car");
+        String make = jsonObject.getString("make");
+        String model = jsonObject.getString("model");
+        int year = jsonObject.getInt("year");
+        return carGenerator.GenerateCarUseCase(make, model, year);
+    }
 }
