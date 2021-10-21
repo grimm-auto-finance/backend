de4c801fba9531e79b70569a26e9ff0e0e4d1e1e
Gabe Guralnick <gnguralnick@gmail.com>
2021-10-20 22:06:09 -0400

    feat: update ParseCarBuyerUseCase
    reads from jsonobject to create CarBuyer object

--- src/main/java/entityparsers/ParseCarBuyerUseCase.java
+++ src/main/java/entityparsers/ParseCarBuyerUseCase.java
@@ -1,7 +1,33 @@
 package entityparsers;
+import entities.CarBuyer;
+import entitybuilder.GenerateBuyerUseCase;
+
+import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
 
 public class ParseCarBuyerUseCase {
 
-    private JsonObjectBuilder builder;
+    private final JsonObject jsonObject;
+
+    /**
+     * Constructs a new ParseCarBuyerUseCase to create a CarBuyer
+     * using the given JsonObject
+     * @param jsonObject
+     */
+    public ParseCarBuyerUseCase(JsonObject jsonObject) {
+        this.jsonObject = jsonObject;
+    }
+
+    /**
+     * Creates a CarBuyer object from the fields in jsonObject
+     * @return
+     */
+    public CarBuyer parse() {
+        GenerateBuyerUseCase buyerGenerator = new GenerateBuyerUseCase();
+
+        JsonObject buyerObj = jsonObject.getJsonObject("car buyer");
+        double budget = buyerObj.getJsonNumber("budget").doubleValue();
+        int creditScore = buyerObj.getInt("credit score");
+        return buyerGenerator.GenerateBuyerDataUseCase(budget, creditScore);
+    }
 }
