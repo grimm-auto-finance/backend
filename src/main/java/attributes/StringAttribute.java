415759c0e000d6f938773f3b54371c07ad5273c1
Gabe Guralnick <gnguralnick@gmail.com>
2021-10-23 19:02:12 -0400

    fix: refactor Attribute classes so names make more sense
    AttributeDouble -> DoubleAttribute, etc.

---
+++ src/main/java/attributes/StringAttribute.java
@@ -0,0 +1,23 @@
+package attributes;
+
+public class StringAttribute extends Attribute {
+
+    // the String item stored in this StringAttribute
+    private final String item;
+
+    /**
+     * Constructs a new StringAttribute with the given String item
+     * @param item
+     */
+    public StringAttribute(String item) {
+        this.item = item;
+    }
+
+    /**
+     * Returns the String item stored in this StringAttribute
+     * @return
+     */
+    public String getAttribute() {
+        return item;
+    }
+}
