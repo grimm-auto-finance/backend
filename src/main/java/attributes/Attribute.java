90c991e33ab77f4f4587683307b3fc6790d8e1ed
Gabe Guralnick <gnguralnick@gmail.com>
2021-10-23 19:03:52 -0400

    feat: add JavaDoc comments to Attribute

--- src/main/java/attributes/Attribute.java
+++ src/main/java/attributes/Attribute.java
@@ -2,8 +2,16 @@ package attributes;
 
 public abstract class Attribute {
 
+    /**
+     * Return the item stored in this Attribute.
+     * @return
+     */
     abstract Object getAttribute();
 
+    /**
+     * Returns a string representation of this Attribute.
+     * @return
+     */
     public String toString() {
         return getAttribute().toString();
     }
