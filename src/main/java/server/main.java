535d46f947463e6beee5e9c722654ff353430001
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-23 20:52:31 -0400

    feat: add DataBaseFetcher

--- src/main/java/server/main.java
+++ src/main/java/server/main.java
@@ -5,11 +5,15 @@ import com.sun.net.httpserver.HttpServer;
 import controllers.Route;
 import controllers.Routes;
 
+import fetchers.DataBaseFetcher;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.sql.SQLException;
 
 class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
+        DataBaseFetcher.connectAndMigrate();
         HttpServer server = HttpServer.create(new InetSocketAddress(Env.PORT), 0);
         for (Route route : Routes.routes) {
             server.createContext(route.getContext(), route);
