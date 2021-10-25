5c96dfce8364545a1f7a99eba6aabb5c433972cb
Gabe Guralnick <gnguralnick@gmail.com>
2021-10-25 12:41:30 -0400

    feat: create AttributizeAddOnUseCase to handle converting AddOns into AttributeMaps

---
+++ src/main/java/entitypackagers/AttributizeAddOnUseCase.java
@@ -0,0 +1,22 @@
+package entitypackagers;
+
+import attributes.AttributeMap;
+import constants.EntityStringNames;
+import entities.AddOn;
+
+public class AttributizeAddOnUseCase {
+
+    private final AddOn addOn;
+
+    public AttributizeAddOnUseCase(AddOn addOn) {
+        this.addOn = addOn;
+    }
+
+    public AttributeMap attributizeEntity() {
+        AttributeMap addOnMap = new AttributeMap();
+        addOnMap.addItem(EntityStringNames.ADD_ON_NAME, addOn.getName());
+        addOnMap.addItem(EntityStringNames.ADD_ON_PRICE, addOn.getPrice());
+        addOnMap.addItem(EntityStringNames.ADD_ON_DESCRIPTION, addOn.getDescription());
+        return addOnMap;
+    }
+}
