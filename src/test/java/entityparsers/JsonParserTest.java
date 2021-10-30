af89c94441676e291d214a5016c0f1d7867b3ff0
Gabe Guralnick <gnguralnick@gmail.com>
2021-10-30 16:58:07 -0400

    feat: add unit tests for JsonParser
    note testParserAllInts is commented out because JsonParser can't currently separate doubles and ints

--- src/test/java/entityparsers/JsonParserTest.java
+++ src/test/java/entityparsers/JsonParserTest.java
@@ -1,10 +1,13 @@
 package entityparsers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import attributes.AttributeMap;
 import constants.Exceptions;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.json.Json;
@@ -13,25 +16,74 @@ import javax.json.JsonObjectBuilder;
 public class JsonParserTest {
 
     static JsonObjectBuilder builder;
+    static AttributeMap testMap;
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    public void setup() {
         builder = Json.createObjectBuilder();
-        builder.add("Int value", 5);
-        builder.add("String value", "hello");
-        builder.add("Double value", 5.5);
-        JsonObjectBuilder subObj = Json.createObjectBuilder();
-        subObj.add("Sub-String value", "sub-string");
-        builder.add("Sub-object", subObj);
+        testMap = new AttributeMap();
+    }
+
+    static void addToBoth(String name, String value) {
+        builder.add(name, value);
+        testMap.addItem(name, value);
+    }
+
+    static void addToBoth(String name, int value) {
+        builder.add(name, value);
+        testMap.addItem(name, value);
+    }
+
+    static void addToBoth(String name, double value) {
+        builder.add(name, value);
+        testMap.addItem(name, value);
+    }
+
+    static void testEq(JsonParser parser) {
+        try {
+            assertEquals(testMap.toString(), parser.parse().toString());
+        } catch (Exceptions.ParseException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testParserAllStrings() {
+        addToBoth("String value", "test value");
+        addToBoth("String 2", "string 2 value");
+        JsonParser parser = new JsonParser(builder.build());
+        testEq(parser);
+    }
+
+    // Fails since JsonParser currently can't find ints
+//    @Test
+//    public void testParserAllInts() {
+//        addToBoth("int value", 16);
+//        addToBoth("int value 2", 18);
+//        JsonParser parser = new JsonParser(builder.build());
+//        testEq(parser);
+//    }
+
+    @Test
+    public void testParserAllDoubles() {
+        addToBoth("double value", 18.0);
+        addToBoth("double value 2", 20.56);
+        JsonParser parser = new JsonParser(builder.build());
+        testEq(parser);
     }
 
     @Test
     public void testJsonParser() {
+       // addToBoth("Int value", 5);
+        addToBoth("String value", "hello");
+        addToBoth("Double value", 5.5);
+        AttributeMap subMap = new AttributeMap();
+        subMap.addItem("Sub-String value", "sub-string");
+        testMap.addItem("Sub-object", subMap);
+        JsonObjectBuilder subBuilder = Json.createObjectBuilder();
+        subBuilder.add("Sub-String value", "sub-string");
+        builder.add("Sub-object", subBuilder);
         JsonParser parser = new JsonParser(builder.build());
-        try {
-            System.out.println(parser.parse());
-        } catch (Exceptions.ParseException e) {
-            fail();
-        }
+        testEq(parser);
     }
 }
