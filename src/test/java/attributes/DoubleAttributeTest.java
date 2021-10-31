f86a3aadf51e83d9d27c632437e94c972a67557b
Mogtaba-Alim <alim.mogtaba@gmail.com>
2021-10-31 02:49:30 -0400

    feat: Created unit test for DoubleAttribute

---
+++ src/test/java/attributes/DoubleAttributeTest.java
@@ -0,0 +1,14 @@
+package attributes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DoubleAttributeTest {
+    @Test
+    public void TestGetAttribute(){
+        DoubleAttribute doubAtt = new DoubleAttribute(1.1);
+        assertEquals(1.1, doubAtt.getAttribute());
+    }
+
+}
