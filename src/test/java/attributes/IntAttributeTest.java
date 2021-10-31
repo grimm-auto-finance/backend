93391179be667f6b7145fdaff00f308b62369816
Mogtaba-Alim <alim.mogtaba@gmail.com>
2021-10-31 02:51:37 -0400

    feat: Created unit test for IntAttribute

---
+++ src/test/java/attributes/IntAttributeTest.java
@@ -0,0 +1,14 @@
+package attributes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntAttributeTest {
+    @Test
+    public void TestGetAttribute(){
+        IntAttribute intAtt = new IntAttribute(1);
+        assertEquals(1, intAtt.getAttribute());
+    }
+
+}
