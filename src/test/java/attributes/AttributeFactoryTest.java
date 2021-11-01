1c58ecac1e750967d1fdb0219da18a05a9bcf45b
Mogtaba-Alim <alim.mogtaba@gmail.com>
2021-10-31 21:22:28 -0400

    fix: reformated the files in attributes to follow google-java-Part 2

--- src/test/java/attributes/AttributeFactoryTest.java
+++ src/test/java/attributes/AttributeFactoryTest.java
@@ -6,14 +6,14 @@ import static attributes.AttributeFactory.getAttribute;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AttributeFactoryTest {
-  String[] cars = {"Audi"};
-  Object[] obj = {1, 1.1, "Hello", cars};
+    String[] cars = {"Audi"};
+    Object[] obj = {1, 1.1, "Hello", cars};
 
-  @Test
-  public void testAttributeFactory() {
+    @Test
+    public void testAttributeFactory() {
 
-    assertEquals(1, getAttribute(obj[0]).getAttribute());
-    assertEquals(1.1, getAttribute(obj[1]).getAttribute());
-    assertEquals("Hello", getAttribute(obj[2]).getAttribute());
-  }
+        assertEquals(1, getAttribute(obj[0]).getAttribute());
+        assertEquals(1.1, getAttribute(obj[1]).getAttribute());
+        assertEquals("Hello", getAttribute(obj[2]).getAttribute());
+    }
 }
--- src/test/java/attributes/DoubleAttributeTest.java
+++ src/test/java/attributes/DoubleAttributeTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DoubleAttributeTest {
-  @Test
-  public void TestGetAttribute() {
-    DoubleAttribute doubAtt = new DoubleAttribute(1.1);
-    assertEquals(1.1, doubAtt.getAttribute());
-  }
+    @Test
+    public void TestGetAttribute() {
+        DoubleAttribute doubAtt = new DoubleAttribute(1.1);
+        assertEquals(1.1, doubAtt.getAttribute());
+    }
 }
