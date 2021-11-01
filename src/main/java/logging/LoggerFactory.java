3f09035e91b8948ccf3188c1a04fe1677f4e91d6
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-10-31 20:44:42 -0400

    feat: add logging package

---
+++ src/main/java/logging/LoggerFactory.java
@@ -0,0 +1,9 @@
+package logging;
+
+/** A class that allows you to create loggers */
+public class LoggerFactory {
+    /** @return a new object that implements logger */
+    public static Logger getLogger() {
+        return (Logger) new SystemLogger();
+    }
+}
