90a8de03d536c9ce0e1b52aeac13417b63fb83bf
Mogtaba-Alim <alim.mogtaba@gmail.com>
2021-10-31 21:09:05 -0400

    fix: Reformatted to google java format

--- src/test/java/attributes/AttributeMapTest.java
+++ src/test/java/attributes/AttributeMapTest.java
@@ -6,44 +6,41 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AttributeMapTest {
 
-    @Test
-    public void TestaddItemAttributeAndGet(){
-        AttributeMap attMap = new AttributeMap();
-        try {
-            attMap.addItem("integer_Attribute", new IntAttribute(1));
-        } catch(ClassCastException e){
-            fail();
-        }
-
-        assertEquals(new IntAttribute(1).getAttribute(),
-                attMap.getItem("integer_Attribute").getAttribute());
-
+  @Test
+  public void TestaddItemAttributeAndGet() {
+    AttributeMap attMap = new AttributeMap();
+    try {
+      attMap.addItem("integer_Attribute", new IntAttribute(1));
+    } catch (ClassCastException e) {
+      fail();
     }
 
-    @Test
-    public void TestaddItemObjectAndGet(){
-        AttributeMap attMap = new AttributeMap();
-        try {
-            attMap.addItem("integer_Object", 1);
-        } catch(ClassCastException e){
-            fail();
-        }
+    assertEquals(
+        new IntAttribute(1).getAttribute(), attMap.getItem("integer_Attribute").getAttribute());
+  }
 
-        assertEquals(new IntAttribute(1).getAttribute(),
-                attMap.getItem("integer_Object").getAttribute());
-
-    }
-    @Test
-    public void TestGetAttribute(){
-        AttributeMap attMap = new AttributeMap();
-        try {
-            attMap.addItem("integer_Attribute", new IntAttribute(1));
-            attMap.addItem("double_Attribute",new DoubleAttribute(1.1));
-        } catch(ClassCastException e){
-            fail();
-        }
-        assertEquals( "{double_Attribute=1.1, integer_Attribute=1}", attMap.getAttribute().toString());
-
+  @Test
+  public void TestaddItemObjectAndGet() {
+    AttributeMap attMap = new AttributeMap();
+    try {
+      attMap.addItem("integer_Object", 1);
+    } catch (ClassCastException e) {
+      fail();
     }
 
+    assertEquals(
+        new IntAttribute(1).getAttribute(), attMap.getItem("integer_Object").getAttribute());
+  }
+
+  @Test
+  public void TestGetAttribute() {
+    AttributeMap attMap = new AttributeMap();
+    try {
+      attMap.addItem("integer_Attribute", new IntAttribute(1));
+      attMap.addItem("double_Attribute", new DoubleAttribute(1.1));
+    } catch (ClassCastException e) {
+      fail();
+    }
+    assertEquals("{double_Attribute=1.1, integer_Attribute=1}", attMap.getAttribute().toString());
+  }
 }
