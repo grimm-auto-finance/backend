497094b0037e79ea54b6aa69da51fc3a40c20f4e
Mogtaba-Alim <alim.mogtaba@gmail.com>
2021-10-31 21:46:47 -0400

    fix: reformated the files in entitiyparsers to follow google-java-Part 2

--- src/test/java/entityparsers/JsonParserTest.java
+++ src/test/java/entityparsers/JsonParserTest.java
@@ -15,81 +15,81 @@ import javax.json.JsonObjectBuilder;
 
 public class JsonParserTest {
 
-  static JsonObjectBuilder builder;
-  static AttributeMap testMap;
+    static JsonObjectBuilder builder;
+    static AttributeMap testMap;
 
-  @BeforeEach
-  public void setup() {
-    builder = Json.createObjectBuilder();
-    testMap = new AttributeMap();
-  }
-
-  /**
-   * Adds the given String value to both builder and testMap with the given name
-   *
-   * @param name
-   * @param value
-   */
-  static void addToBoth(String name, String value) {
-    builder.add(name, value);
-    testMap.addItem(name, value);
-  }
-
-  static void addToBoth(String name, int value) {
-    builder.add(name, value);
-    testMap.addItem(name, value);
-  }
-
-  static void addToBoth(String name, double value) {
-    builder.add(name, value);
-    testMap.addItem(name, value);
-  }
-
-  static void testEq(JsonParser parser) {
-    try {
-      assertEquals(testMap.toString(), parser.parse().toString());
-    } catch (Exceptions.ParseException e) {
-      fail();
+    @BeforeEach
+    public void setup() {
+        builder = Json.createObjectBuilder();
+        testMap = new AttributeMap();
     }
-  }
 
-  @Test
-  public void testParserAllStrings() {
-    addToBoth("String value", "test value");
-    addToBoth("String 2", "string 2 value");
-    JsonParser parser = new JsonParser(builder.build());
-    testEq(parser);
-  }
+    /**
+     * Adds the given String value to both builder and testMap with the given name
+     *
+     * @param name
+     * @param value
+     */
+    static void addToBoth(String name, String value) {
+        builder.add(name, value);
+        testMap.addItem(name, value);
+    }
 
-  // Fails since JsonParser currently treats all numbers as doubles
-  //    @Test
-  //    public void testParserAllInts() {
-  //        addToBoth("int value", 16);
-  //        addToBoth("int value 2", 18);
-  //        JsonParser parser = new JsonParser(builder.build());
-  //        testEq(parser);
-  //    }
+    static void addToBoth(String name, int value) {
+        builder.add(name, value);
+        testMap.addItem(name, value);
+    }
 
-  @Test
-  public void testParserAllDoubles() {
-    addToBoth("double value", 18.0);
-    addToBoth("double value 2", 20.56);
-    JsonParser parser = new JsonParser(builder.build());
-    testEq(parser);
-  }
+    static void addToBoth(String name, double value) {
+        builder.add(name, value);
+        testMap.addItem(name, value);
+    }
 
-  @Test
-  public void testJsonParser() {
-    // addToBoth("Int value", 5);
-    addToBoth("String value", "hello");
-    addToBoth("Double value", 5.5);
-    AttributeMap subMap = new AttributeMap();
-    subMap.addItem("Sub-String value", "sub-string");
-    testMap.addItem("Sub-object", subMap);
-    JsonObjectBuilder subBuilder = Json.createObjectBuilder();
-    subBuilder.add("Sub-String value", "sub-string");
-    builder.add("Sub-object", subBuilder);
-    JsonParser parser = new JsonParser(builder.build());
-    testEq(parser);
-  }
+    static void testEq(JsonParser parser) {
+        try {
+            assertEquals(testMap.toString(), parser.parse().toString());
+        } catch (Exceptions.ParseException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testParserAllStrings() {
+        addToBoth("String value", "test value");
+        addToBoth("String 2", "string 2 value");
+        JsonParser parser = new JsonParser(builder.build());
+        testEq(parser);
+    }
+
+    // Fails since JsonParser currently treats all numbers as doubles
+    //    @Test
+    //    public void testParserAllInts() {
+    //        addToBoth("int value", 16);
+    //        addToBoth("int value 2", 18);
+    //        JsonParser parser = new JsonParser(builder.build());
+    //        testEq(parser);
+    //    }
+
+    @Test
+    public void testParserAllDoubles() {
+        addToBoth("double value", 18.0);
+        addToBoth("double value 2", 20.56);
+        JsonParser parser = new JsonParser(builder.build());
+        testEq(parser);
+    }
+
+    @Test
+    public void testJsonParser() {
+        // addToBoth("Int value", 5);
+        addToBoth("String value", "hello");
+        addToBoth("Double value", 5.5);
+        AttributeMap subMap = new AttributeMap();
+        subMap.addItem("Sub-String value", "sub-string");
+        testMap.addItem("Sub-object", subMap);
+        JsonObjectBuilder subBuilder = Json.createObjectBuilder();
+        subBuilder.add("Sub-String value", "sub-string");
+        builder.add("Sub-object", subBuilder);
+        JsonParser parser = new JsonParser(builder.build());
+        testEq(parser);
+    }
 }
