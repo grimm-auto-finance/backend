921bb80b4ad2f2fb9a480be91ca5e6e6b4225efd
Gabe Guralnick <gnguralnick@gmail.com>
2021-11-02 12:43:50 -0400

    feat: create JsonPackageTest to test features of JsonPackage

---
+++ src/test/java/entitypackagers/JsonPackageTest.java
@@ -0,0 +1,32 @@
+package entitypackagers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
+public class JsonPackageTest {
+
+    static JsonObject obj;
+
+    @BeforeEach
+    public void setup() {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        builder.add("test 1", "test 1");
+        builder.add("test 2", 2);
+        builder.add("test 3", 3.5);
+        JsonObjectBuilder subBuilder = Json.createObjectBuilder();
+        subBuilder.add("sub test", "sub test");
+        builder.add("sub builder", subBuilder);
+        obj = builder.build();
+    }
+
+    @Test
+    public void testJSONPackageGetPackage() {
+        JsonPackage jsonPackage = new JsonPackage(obj);
+        Assertions.assertEquals(obj, jsonPackage.getPackage());
+    }
+}
