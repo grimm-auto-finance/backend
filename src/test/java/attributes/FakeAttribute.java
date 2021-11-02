72da4deeb94febe3838be6bb17a6488a5e62f86e
Gabe Guralnick <gnguralnick@gmail.com>
2021-11-02 15:51:30 -0400

    feat: create FakeAttribute
    used in tests to ensure classes handling Attributes throw the right exceptions when an unhandled Attribute is given

---
+++ src/test/java/attributes/FakeAttribute.java
@@ -0,0 +1,9 @@
+package attributes;
+
+public class FakeAttribute extends Attribute {
+    private final Object item;
+
+    public FakeAttribute(Object item) { this.item = item; }
+
+    public Object getAttribute() { return this.item; }
+}
