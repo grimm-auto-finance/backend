fc472db313f85f1080c4ed81a6016f50888c565c
Gabe Guralnick <gnguralnick@gmail.com>
2021-11-02 22:47:34 -0400

    feat: create AttributizeCarBuyerUseCaseTest

---
+++ src/test/java/entitypackagers/AttributizeCarBuyerUseCaseTest.java
@@ -0,0 +1,34 @@
+package entitypackagers;
+
+import attributes.AttributeMap;
+import constants.EntityStringNames;
+import entities.CarBuyer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AttributizeCarBuyerUseCaseTest {
+
+    static AttributeMap testMap;
+    static CarBuyer buyer;
+    static AttributizeCarBuyerUseCase buyerAttributizer;
+
+    @BeforeEach
+    public void setup() {
+        buyer = new CarBuyer(30000.5, 750);
+        testMap = new AttributeMap();
+        buyerAttributizer = new AttributizeCarBuyerUseCase(buyer);
+        addBuyerToTestMap();
+    }
+
+    static void addBuyerToTestMap() {
+        testMap.addItem(EntityStringNames.BUYER_BUDGET, buyer.getBudget());
+        testMap.addItem(EntityStringNames.BUYER_CREDIT, buyer.getCreditScore());
+    }
+
+    @Test
+    public void testAttributizeCarBuyer() {
+        assertEquals(testMap.getAttribute().toString(), buyerAttributizer.attributizeEntity().getAttribute().toString());
+    }
+}
