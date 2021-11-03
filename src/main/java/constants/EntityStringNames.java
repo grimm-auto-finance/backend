6a4d5770005ada111b104b127ccac73fc443aae3
Mogtaba-Alim <alim.mogtaba@gmail.com>
2021-11-03 13:56:42 -0400

    fix: refactored the files in constants and reformated to follow google-java

--- src/main/java/constants/EntityStringNames.java
+++ src/main/java/constants/EntityStringNames.java
@@ -1,19 +1,26 @@
 package constants;
 
+/** String representations of entity instance variable names */
 public class EntityStringNames {
 
+    /** String representation of the instance variable names in the Car entity */
     public static final String CAR_STRING = "car";
+
     public static final String CAR_MAKE = "make";
     public static final String CAR_MODEL = "model";
     public static final String CAR_YEAR = "year";
     public static final String CAR_PRICE = "price";
     public static final String CAR_ADD_ONS = "addOns";
 
+    /** String representation of the instance variable names in the CarBuyer entity */
     public static final String BUYER_STRING = "car buyer";
+
     public static final String BUYER_BUDGET = "pytBudget";
     public static final String BUYER_CREDIT = "creditScore";
 
+    /** String representation of the instance variable names in the LoanData entity */
     public static final String LOAN_STRING = "loan";
+
     public static final String LOAN_INTEREST_RATE = "interestRate";
     public static final String LOAN_INSTALLMENT = "installment";
     public static final String LOAN_SCORE = "sensoScore";
@@ -21,7 +28,9 @@ public class EntityStringNames {
     public static final String LOAN_TERM_LENGTH = "termLength";
     public static final String LOAN_INTEREST_SUM = "interestSum";
 
+    /** String representation of the instance variable names in the AddOn entity */
     public static final String ADD_ON_STRING = "addOn";
+
     public static final String ADD_ON_NAME = "name";
     public static final String ADD_ON_PRICE = "price";
     public static final String ADD_ON_DESCRIPTION = "description";
--- src/main/java/constants/Exceptions.java
+++ src/main/java/constants/Exceptions.java
@@ -1,9 +1,11 @@
 package constants;
 
+/**
+ * A class representing the exceptions that are unique to this project and not prepackaged with Java
+ */
 public class Exceptions {
+    /** Abstract exception parent class which is inherited by other project specific exceptions */
     public abstract static class CodedException extends Exception {
-        public abstract int getCode();
-
         public CodedException() {
             super();
         }
@@ -11,53 +13,71 @@ public class Exceptions {
         public CodedException(String message) {
             super(message);
         }
+
+        public abstract int getCode();
     }
 
+    /** FetchException class that is thrown when when a fetch request is made */
     public static class FetchException extends CodedException {
-        public int getCode() {
-            return 500;
-        }
-
         public FetchException() {}
 
+        /** @param message The exception message */
         public FetchException(String message) {
             super(message);
         }
+
+        /** @return Returns the specifc FetchException code */
+        public int getCode() {
+            return 500;
+        }
     }
 
+    /** ParseException class that is thrown whenever a Json object is parsed */
     public static class ParseException extends CodedException {
-        public int getCode() {
-            return 400;
-        }
-
         public ParseException() {}
 
+        /** @param message The exception message */
         public ParseException(String message) {
             super(message);
         }
-    }
 
-    public static class PackageException extends CodedException {
+        /** @return Returns the specific ParseException code */
         public int getCode() {
             return 400;
         }
+    }
 
+    /**
+     * PackageException class that is thrown whenever a java object is packaged into a Json object
+     */
+    public static class PackageException extends CodedException {
         public PackageException() {}
 
+        /** @param message The exception message */
         public PackageException(String message) {
             super(message);
         }
-    }
 
-    public static class MissingMethodException extends CodedException {
+        /** @return Returns the specific PackageException code */
         public int getCode() {
-            return 404;
+            return 400;
         }
-
+    }
+    /**
+     * MissingMethodException class that is used only in the routes and thrown whenever a method is
+     * missing
+     */
+    public static class MissingMethodException extends CodedException {
         public MissingMethodException() {}
 
+        /** @param message The exception message */
         public MissingMethodException(String message) {
             super(message);
+        }
+
+        /** @return Returns the specific MissingMethodException code */
+        public int getCode() {
+            return 404;
         }
     }
 }
