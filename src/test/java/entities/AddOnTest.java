8af4a0868cb7ae3a062195a5e50fc718cb510be1
Gabe Guralnick <gnguralnick@gmail.com>
2021-11-03 12:16:12 -0400

    fix: remove incomplete constructor for AddOns
    having uninitialized instance variables was causing lots of nullpointererrors and we never even used the incomplete constructor in a meaningful way

--- src/test/java/entities/AddOnTest.java
+++ src/test/java/entities/AddOnTest.java
@@ -6,16 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
 
 public class AddOnTest {
-    @Test
-    public void gettersAndSetters() {
-        AddOn addOn = new AddOn("Marshmallows");
-        assertEquals("Marshmallows", addOn.getName());
-        assertEquals(0.0, addOn.getPrice());
-        assertNull(addOn.getDescription());
-        addOn.setPrice(5.59);
-        addOn.setDescription("Very Tasty");
-        assertEquals("Marshmallows", addOn.getName());
-        assertEquals(5.59, addOn.getPrice());
-        assertEquals("Very Tasty", addOn.getDescription());
-    }
+//    @Test
+//    public void gettersAndSetters() {
+//        AddOn addOn = new AddOn("Marshmallows");
+//        assertEquals("Marshmallows", addOn.getName());
+//        assertEquals(0.0, addOn.getPrice());
+//        assertNull(addOn.getDescription());
+//        addOn.setPrice(5.59);
+//        addOn.setDescription("Very Tasty");
+//        assertEquals("Marshmallows", addOn.getName());
+//        assertEquals(5.59, addOn.getPrice());
+//        assertEquals("Very Tasty", addOn.getDescription());
+//    }
 }
