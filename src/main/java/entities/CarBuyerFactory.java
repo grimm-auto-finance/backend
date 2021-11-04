50df7fafe60cea56708aa42e5b547a2f3d850017
Gabe Guralnick <gnguralnick@gmail.com>
2021-11-04 16:26:53 -0400

    feat: create CarBuyerFactory to handle creating CarBuyers from AttributeMaps

---
+++ src/main/java/entities/CarBuyerFactory.java
@@ -0,0 +1,29 @@
+package entities;
+
+import attributes.AttributeMap;
+import constants.EntityStringNames;
+import constants.Exceptions;
+import entitybuilder.GenerateBuyerUseCase;
+
+public class CarBuyerFactory implements EntityFactory {
+
+    public CarBuyer getEntity(AttributeMap map) throws Exceptions.FactoryException {
+        double budget;
+        int creditScore;
+        try {
+            budget = (double) map.getItem(EntityStringNames.BUYER_BUDGET).getAttribute();
+            creditScore =
+                    (int)
+                            Math.round(
+                                    (Double)
+                                            map.getItem(EntityStringNames.BUYER_CREDIT)
+                                                    .getAttribute());
+        } catch (ClassCastException | NullPointerException e) {
+            Exceptions.FactoryException ex = new Exceptions.FactoryException(e.getMessage());
+            ex.setStackTrace(e.getStackTrace());
+            throw ex;
+        }
+
+        return new CarBuyer(budget, creditScore);
+    }
+}
