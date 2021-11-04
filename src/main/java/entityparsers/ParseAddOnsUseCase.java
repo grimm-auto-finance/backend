2b239c41904f7e2cffcee587dc5b84d579f8772f
ianlavine <71903686+ianlavine@users.noreply.github.com>
2021-11-03 20:53:57 -0400

    fix: Constructor now creates a list of AddOn AttributeMaps, and parse method now returns a mapping of Strings to Addons

--- src/main/java/entityparsers/ParseAddOnsUseCase.java
+++ src/main/java/entityparsers/ParseAddOnsUseCase.java
@@ -1,17 +1,31 @@
 package entityparsers;
 
-import javax.json.JsonObject;
+import attributes.AttributeMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import constants.EntityStringNames;
+import constants.Exceptions;
+
+import entities.AddOn;
+
+import entitybuilder.GenerateAddOnsUseCase;
+import entitybuilder.GenerateCarUseCase;
 
 public class ParseAddOnsUseCase {
-    private final JsonObject jsonObject;
+
+    private final ArrayList<AttributeMap> addonList;
 
     /**
-     * Constructs a new ParseAddOnsUseCase to create a AddOns using the given JsonObject
+     * Constructs a new ParseCarUseCase to create a Car using the given Parser
      *
-     * @param jsonObject
+     * @param parser object to be parsed
      */
-    public ParseAddOnsUseCase(JsonObject jsonObject) {
-        this.jsonObject = jsonObject;
+    public ParseAddOnsUseCase(ArrayList<Parser> parser) throws Exceptions.ParseException {
+        addonList = new ArrayList<AttributeMap>();
+        for(Parser p: parser) {
+            map.add(p.parse());
+        }
     }
 
     /**
@@ -20,10 +34,25 @@ public class ParseAddOnsUseCase {
      * @return
      */
     //    TODO: Uncomment ParseAddOnsUseCase.parse() when this is implemented
-    //    public Map<String, AddOn> parse() {
-    //        GenerateAddOnsUseCase addOnsGenerator = new GenerateAddOnsUseCase();
-    //        JsonObject addOnsObject = jsonObject.getJsonObject("add-ons");
-    //
-    //        return addOnsGenerator.GenerateAddOnsUseCase();
-    //    }
+    public HashMap<String, AddOn> parse() {
+        HashMap<String, AttributeMap> addOns = new HashMap<String, AttributeMap>();
+        GenerateAddOnsUseCase AddOnGenerator = new GenerateAddOnsUseCase();
+        for (AttributeMap a : addonList) {
+            String name, description;
+            double price;
+            AttributeMap addOnMap;
+            try {
+                addOnMap = (AttributeMap) a.getItem(EntityStringNames.ADD_ON_STRING);
+                name = (String) addOnMap.getItem(EntityStringNames.ADD_ON_NAME).getAttribute();
+                price = (double) addOnMap.getItem(EntityStringNames.ADD_ON_PRICE).getAttribute();
+                description = (String) addOnMap.getItem(EntityStringNames.ADD_ON_DESCRIPTION).getAttribute();
+            } catch (ClassCastException e) {
+                Exceptions.ParseException ex = new Exceptions.ParseException(e.getMessage());
+                ex.setStackTrace(e.getStackTrace());
+                throw ex;
+            }
+            addOns.put(name, addOnMap);
+        }
+        return AddOnGenerator.GenerateAddOnDataUseCase(addOns);
+    }
 }
