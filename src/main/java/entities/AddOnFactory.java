e33ef875bb7625135b69ee7af62ab3a687847c9d
Gabe Guralnick <gnguralnick@gmail.com>
2021-11-04 16:30:12 -0400

    feat: create AddOnFactory to handle creation of AddOns from AttributeMap

---
+++ src/main/java/entities/AddOnFactory.java
@@ -0,0 +1,25 @@
+package entities;
+
+import attributes.AttributeMap;
+import constants.EntityStringNames;
+import constants.Exceptions;
+
+public class AddOnFactory implements EntityFactory {
+
+    public AddOn getEntity(AttributeMap map) throws Exceptions.FactoryException {
+        String name;
+        double price;
+        String description;
+        try {
+            name = (String) map.getItem(EntityStringNames.ADD_ON_NAME).getAttribute();
+            price = (double) map.getItem(EntityStringNames.ADD_ON_PRICE).getAttribute();
+            description = (String) map.getItem(EntityStringNames.ADD_ON_DESCRIPTION).getAttribute();
+        } catch (ClassCastException | NullPointerException e) {
+            Exceptions.FactoryException ex = new Exceptions.FactoryException(e.getMessage());
+            ex.setStackTrace(e.getStackTrace());
+            throw ex;
+        }
+
+        return new AddOn(name, price, description);
+    }
+}
