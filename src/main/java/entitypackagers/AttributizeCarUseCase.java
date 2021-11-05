22d5de402f0d206827d7fd7ff25e70c8a140c300
Gabe Guralnick <gnguralnick@gmail.com>
2021-11-04 22:53:49 -0400

    feat: update AttributizeCarUseCase to send AddOns as an ArrayAttribute

--- src/main/java/entitypackagers/AttributizeCarUseCase.java
+++ src/main/java/entitypackagers/AttributizeCarUseCase.java
@@ -10,6 +10,7 @@ import constants.EntityStringNames;
 import entities.AddOn;
 import entities.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,15 +34,20 @@ public class AttributizeCarUseCase implements Attributizer {
         carMap.addItem(EntityStringNames.CAR_MODEL, car.getModel());
         carMap.addItem(EntityStringNames.CAR_YEAR, car.getYear());
 
-        List<AddOn> addOns = car.getAddOns();
-        Attribute[] addOnMaps = new Attribute[addOns.size()];
-        for (int i = 0; i < addOns.size(); i++) {
-            AttributizeAddOnUseCase addOnAttributizer = new AttributizeAddOnUseCase(addOns.get(i));
-            addOnMaps[i] = addOnAttributizer.attributizeEntity();
-        }
+        Map<String, AddOn> addOns = car.getAddOns();
+        Attribute[] addOnMaps = getAddOnAttArray(addOns);
         Attribute addOnArray = AttributeFactory.createAttribute(addOnMaps);
 
         carMap.addItem(EntityStringNames.CAR_ADD_ONS, addOnArray);
         return carMap;
+    }
+
+    public static Attribute[] getAddOnAttArray(Map<String, AddOn> addOns) {
+        List<Attribute> addOnMapsList = new ArrayList<>();
+        for (String s : addOns.keySet()) {
+            AttributizeAddOnUseCase addOnAttributizer = new AttributizeAddOnUseCase(addOns.get(s));
+            addOnMapsList.add(addOnAttributizer.attributizeEntity());
+        }
+        return addOnMapsList.toArray(new Attribute[0]);
     }
 }
