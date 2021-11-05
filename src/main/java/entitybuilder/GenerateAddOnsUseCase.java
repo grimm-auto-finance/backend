706593898718d33477d552a990f12588256322f7
Gabe Guralnick <gnguralnick@gmail.com>
2021-11-05 17:13:18 -0400

    feat: update ParseAddOnsUseCase (finally)
    returns a List of AddOns pulled from an ArrayAttribute stored in the AttributeMap returned from a Parser

--- src/main/java/entitybuilder/GenerateAddOnsUseCase.java
+++ src/main/java/entitybuilder/GenerateAddOnsUseCase.java
@@ -1,24 +1,19 @@
 package entitybuilder;
 
-/** A class to generate a mapping of addon names to AddOn objects with the coressponding name */
+import entities.AddOn;
+
+/** A class to generate AddOn objects */
 public class GenerateAddOnsUseCase {
+
     /**
-     * Takes a list of addon names and generates a mapping of addon names to their corresponding
-     * AddOn objects
+     * generates an object using the given name, price, and description
      *
-     * @param ListOfAddOnNames A list containg names of Addons that we want to generate
-     * @return A mapping of addon name to its corresponding AddOn object
+     * @param name the name of this AddOn
+     * @param price the price of this AddOn
+     * @param description the description of this AddOn
+     * @return an AddOn object constructed using the given parameters
      */
-    //    public HashMap<String, AddOn> GenerateAddOnsUseCase(List<String> ListOfAddOnNames) {
-    //        /**
-    //         * A mapping of the name of the addons we get from the list of addons in our parameter
-    // to
-    //         * the AddOn objects with the corresponding name which we create below
-    //         */
-    //        HashMap<String, AddOn> MapOfAddOns = new HashMap<String, AddOn>();
-    //        for (String addOnName : ListOfAddOnNames) {
-    //            MapOfAddOns.put(addOnName, new AddOn(addOnName));
-    //        }
-    //        return MapOfAddOns;
-    //    }
+    public AddOn generateAddOn(String name, double price, String description) {
+        return new AddOn(name, price, description);
+    }
 }
--- src/main/java/entityparsers/ParseAddOnsUseCase.java
+++ src/main/java/entityparsers/ParseAddOnsUseCase.java
@@ -1,29 +1,53 @@
 package entityparsers;
 
+import attributes.ArrayAttribute;
+import attributes.Attribute;
+import attributes.AttributeMap;
+import constants.EntityStringNames;
+import constants.Exceptions;
+import entities.AddOn;
+import entitybuilder.GenerateAddOnsUseCase;
+
 import javax.json.JsonObject;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParseAddOnsUseCase {
-    private final JsonObject jsonObject;
+    private final AttributeMap map;
 
     /**
-     * Constructs a new ParseAddOnsUseCase to create a AddOns using the given JsonObject
+     * Constructs a new ParseAddOnsUseCase to create a list of AddOns using the given Parser
      *
-     * @param jsonObject
+     * @param parser the Parser to use to create the AttributeMap for these AddOns
      */
-    public ParseAddOnsUseCase(JsonObject jsonObject) {
-        this.jsonObject = jsonObject;
+    public ParseAddOnsUseCase(Parser parser) throws Exceptions.ParseException {
+        this.map = parser.parse();
     }
 
     /**
-     * Creates a map from add-on names to AddOn objects from the fields in jsonObject
-     *
-     * @return
+     * Creates a List of AddOns parsed from the AttributeMap returned by parser
+     * @return a List of AddOns
+     * @throws Exceptions.ParseException if the appropriate values aren't present in the AttributeMap or have the wrong types
      */
-    //    TODO: Uncomment ParseAddOnsUseCase.parse() when this is implemented
-    //    public Map<String, AddOn> parse() {
-    //        GenerateAddOnsUseCase addOnsGenerator = new GenerateAddOnsUseCase();
-    //        JsonObject addOnsObject = jsonObject.getJsonObject("add-ons");
-    //
-    //        return addOnsGenerator.GenerateAddOnsUseCase();
-    //    }
+    public List<AddOn> parse() throws Exceptions.ParseException {
+        GenerateAddOnsUseCase addOnGenerator = new GenerateAddOnsUseCase();
+        List<AddOn> addOns = new ArrayList<>();
+        try {
+            ArrayAttribute addOnArrayAtt = (ArrayAttribute) map.getItem(EntityStringNames.ADD_ON_STRING);
+            Attribute[] addOnArray = addOnArrayAtt.getAttribute();
+            for (Attribute attribute : addOnArray) {
+                AttributeMap addOnMap = (AttributeMap) attribute;
+                String name = (String) addOnMap.getItem(EntityStringNames.ADD_ON_NAME).getAttribute();
+                String description = (String) addOnMap.getItem(EntityStringNames.ADD_ON_DESCRIPTION).getAttribute();
+                double price = (double) addOnMap.getItem(EntityStringNames.ADD_ON_PRICE).getAttribute();
+                addOns.add(addOnGenerator.generateAddOn(name, price, description));
+            }
+        } catch (ClassCastException | NullPointerException e) {
+            Exceptions.ParseException ex = new Exceptions.ParseException(e.getMessage());
+            ex.setStackTrace(e.getStackTrace());
+            throw ex;
+        }
+
+        return addOns;
+    }
 }
