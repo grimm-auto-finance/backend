a94d5f22d06d89e887df5077fe2a8321991af6f6
Mogtaba-Alim <alim.mogtaba@gmail.com>
2021-11-09 20:34:29 -0500

    fix: format with gfmt

--- src/main/java/entities/Car.java
+++ src/main/java/entities/Car.java
@@ -84,10 +84,7 @@ public class Car extends Entity {
         addOns.remove(addOnName);
     }
 
-    /**
-    *
-    * @return A a hasmap of the cars addons
-    */
+    /** @return A a hasmap of the cars addons */
     public Map<String, AddOn> getAddOns() {
         return new HashMap<>(this.addOns);
     }
@@ -137,10 +134,7 @@ public class Car extends Entity {
         return year;
     }
 
-    /**
-    *
-    * @return Returns the string name of the car 
-    */
+    /** @return Returns the string name of the car */
     @Override
     public String getStringName() {
         return EntityStringNames.CAR_STRING;
