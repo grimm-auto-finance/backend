aa8c3d35c8795dc1ff412d2ce4a4770ba2ce2316
Gabe Guralnick <gnguralnick@gmail.com>
2021-11-30 15:33:38 -0500

    fix: ensure exceptions are thrown properly

--- src/main/java/entityparsers/ParseJsonUseCase.java
+++ src/main/java/entityparsers/ParseJsonUseCase.java
@@ -4,17 +4,24 @@ import attributes.AttributeMap;
 
 import constants.Exceptions;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import javax.json.Json;
+import javax.json.JsonException;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 
 public class ParseJsonUseCase {
 
     public AttributeMap parseJson(InputStream is) throws Exceptions.ParseException {
-        JsonReader jsonReader = Json.createReader(is);
-        JsonObject inputObj = jsonReader.readObject();
+        JsonObject inputObj;
+        try {
+            JsonReader jsonReader = Json.createReader(is);
+            inputObj = jsonReader.readObject();
+        } catch (JsonException e) {
+            throw new Exceptions.ParseException("failed to convert inputstream to JsonObject", e);
+        }
         Parser jsonParser = new JsonParser(inputObj);
         return jsonParser.parse();
     }
